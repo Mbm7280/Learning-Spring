ee2b88d0341113c84d65d2dcfa937202b1545dae
Echo <56158949+Mbm7280@users.noreply.github.com>
2025-05-08 17:30:01 +0800

    init-commit

---
+++ spring-context/src/main/java/org/springframework/context/support/AbstractXmlApplicationContext.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2002-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.context.support;
+
+import java.io.IOException;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.BeanDefinitionDocumentReader;
+import org.springframework.beans.factory.xml.ResourceEntityResolver;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.lang.Nullable;
+
+/**
+ * Convenient base class for {@link org.springframework.context.ApplicationContext}
+ * implementations, drawing configuration from XML documents containing bean definitions
+ * understood by an {@link org.springframework.beans.factory.xml.XmlBeanDefinitionReader}.
+ *
+ * <p>Subclasses just have to implement the {@link #getConfigResources} and/or
+ * the {@link #getConfigLocations} method. Furthermore, they might override
+ * the {@link #getResourceByPath} hook to interpret relative paths in an
+ * environment-specific fashion, and/or {@link #getResourcePatternResolver}
+ * for extended pattern resolution.
+ *
+ * @author Rod Johnson
+ * @author Juergen Hoeller
+ * @see #getConfigResources
+ * @see #getConfigLocations
+ * @see org.springframework.beans.factory.xml.XmlBeanDefinitionReader
+ */
+public abstract class AbstractXmlApplicationContext extends AbstractRefreshableConfigApplicationContext {
+
+	private boolean validating = true;
+
+
+	/**
+	 * Create a new AbstractXmlApplicationContext with no parent.
+	 */
+	public AbstractXmlApplicationContext() {
+	}
+
+	/**
+	 * Create a new AbstractXmlApplicationContext with the given parent context.
+	 * @param parent the parent context
+	 */
+	public AbstractXmlApplicationContext(@Nullable ApplicationContext parent) {
+		super(parent);
+	}
+
+
+	/**
+	 * Set whether to use XML validation. Default is {@code true}.
+	 */
+	public void setValidating(boolean validating) {
+		this.validating = validating;
+	}
+
+
+	/**
+	 * Loads the bean definitions via an XmlBeanDefinitionReader.
+	 * @see org.springframework.beans.factory.xml.XmlBeanDefinitionReader
+	 * @see #initBeanDefinitionReader
+	 * @see #loadBeanDefinitions
+	 */
+
+	// 核心：Bean的加载
+	@Override
+	protected void loadBeanDefinitions(DefaultListableBeanFactory beanFactory) throws BeansException, IOException {
+		// 创建一个xml的beanDefinitionReader 用于解析 xml 配置信息，并对 ResourceLoader 对象 进行赋值
+		XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+
+		// 准备工作：设置 Environment 对象，其中包含当前系统的信息
+		beanDefinitionReader.setEnvironment(getEnvironment());
+		// 准备工作：把 resourceLoader 设置进去,
+		beanDefinitionReader.setResourceLoader(this);
+		// 准备工作：创建实体处理器，用于处理 dtd 和 schema 的解析工作。
+		beanDefinitionReader.setEntityResolver(new ResourceEntityResolver(this));
+		// 准备工作：此处设置配置文件是否要进行验证。并且设置 在 实际解析 xml时 采用 VALIDATION_AUTO 方式，根据逻辑判断使用 DTD 或者 XSD 的方式
+		initBeanDefinitionReader(beanDefinitionReader);
+		// 核心工作：读取并解析 xml，定义成 BeanDefinition 对象
+		// 按照以下顺序 递归解析 string[]-> string -> resource[] -> resource
+		loadBeanDefinitions(beanDefinitionReader);
+	}
+
+	/**
+	 * Initialize the bean definition reader used for loading the bean definitions
+	 * of this context. The default implementation sets the validating flag.
+	 * <p>Can be overridden in subclasses, e.g. for turning off XML validation
+	 * or using a different {@link BeanDefinitionDocumentReader} implementation.
+	 * @param reader the bean definition reader used by this context
+	 * @see XmlBeanDefinitionReader#setValidating
+	 * @see XmlBeanDefinitionReader#setDocumentReaderClass
+	 */
+	protected void initBeanDefinitionReader(XmlBeanDefinitionReader reader) {
+		reader.setValidating(this.validating);
+	}
+
+	/**
+	 * Load the bean definitions with the given XmlBeanDefinitionReader.
+	 * <p>The lifecycle of the bean factory is handled by the {@link #refreshBeanFactory}
+	 * method; hence this method is just supposed to load and/or register bean definitions.
+	 * @param reader the XmlBeanDefinitionReader to use
+	 * @throws BeansException in case of bean registration errors
+	 * @throws IOException if the required XML document isn't found
+	 * @see #refreshBeanFactory
+	 * @see #getConfigLocations
+	 * @see #getResources
+	 * @see #getResourcePatternResolver
+	 */
+	protected void loadBeanDefinitions(XmlBeanDefinitionReader reader) throws BeansException, IOException {
+		Resource[] configResources = getConfigResources();
+		if (configResources != null) {
+			reader.loadBeanDefinitions(configResources);
+		}
+		String[] configLocations = getConfigLocations();
+		if (configLocations != null) {
+			reader.loadBeanDefinitions(configLocations);
+		}
+	}
+
+	/**
+	 * Return an array of Resource objects, referring to the XML bean definition
+	 * files that this context should be built with.
+	 * <p>The default implementation returns {@code null}. Subclasses can override
+	 * this to provide pre-built Resource objects rather than location Strings.
+	 * @return an array of Resource objects, or {@code null} if none
+	 * @see #getConfigLocations()
+	 */
+	@Nullable
+	protected Resource[] getConfigResources() {
+		return null;
+	}
+
+}
